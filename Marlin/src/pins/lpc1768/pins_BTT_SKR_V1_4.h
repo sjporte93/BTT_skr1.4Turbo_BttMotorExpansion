bcfdb22b1c77f2463d59b115b6dac9d21b3f3671
Samuel Porter <74512445+sjporte93@users.noreply.github.com>
2021-06-23 10:13:54 +0100

    updated pins_BTT_SKR_V1_4.h
    corrected DWIN_CREALITY_LCD to show that it's using the EXP pins, therefore conflicting with the btt motor expansion module

--- Marlin/src/pins/lpc1768/pins_BTT_SKR_V1_4.h
+++ Marlin/src/pins/lpc1768/pins_BTT_SKR_V1_4.h
@@ -288,12 +288,12 @@
 #elif ENABLED(DWIN_CREALITY_LCD)
 
   // RET6 DWIN ENCODER LCD
-  #define BTN_ENC                          P1_20
-  #define BTN_EN1                          P1_23
-  #define BTN_EN2                          P1_22
+  #define BTN_ENC                          EXP1_06_PIN //P1_20
+  #define BTN_EN1                          EXP1_03_PIN //P1_23
+  #define BTN_EN2                          EXP1_04_PIN //P1_22
 
   #ifndef BEEPER_PIN
-    #define BEEPER_PIN                     P1_21
+    #define BEEPER_PIN                     EXP1_05_PIN //P1_21
     #undef SPEAKER
   #endif
 
